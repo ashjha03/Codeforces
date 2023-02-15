e51716545b2903e8d8cb2932eda4fb856923c652
Ashish Jha <88213707+ashjha03@users.noreply.github.com>
2023-02-15 14:42:20 +0530

    1341 B

---
+++  Nastya and Door.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, j, n)    for(ll i=j; i<n; i++)
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define vll vector<ll>
+#define vvll vector<vll>
+#define vs vector<string>
+#define pii pair<int, int>
+#define mpi map<int, int>
+#define mpl map<ll, ll>
+#define sti set<int>
+#define stl set<ll>
+#define all(v) v.begin(), v.end()
+#define input(a, n)            \
+    for (ll i = 0; i < n; i++) \
+        cin >> a[i];
+#define output(a, n)           \
+    for (ll i = 0; i < n; i++) \
+        cout << a[i] << " ";
+#define start   \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);
+#define nl cout << "\n"
+#define yes  cout<<"YES"<<endl
+#define no  cout<<"NO"<<endl
+#define prt(x)     cout<<x<<endl
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+int main() {
+    start;
+    ll t; cin>>t;
+    while(t--){
+        ll n, k; cin>>n>>k;
+        vll v(n); input(v, n);
+        ll peaks = 0;
+        rep(i, 1, k-1){
+            if((v[i] > v[i-1]) and (v[i] > v[i+1])) peaks++;
+        }
+        pair<ll, ll> res(peaks, 1);
+        rep(i, k, n){
+            if( (i-k+1)>0 and ( (v[i-k+1]>v[i-k]) and (v[i-k+1]>v[i-k+2]) ) )   peaks--;
+            if((v[i-1] > v[i-2]) and (v[i-1] > v[i])) peaks++;
+            if(res.first < peaks){
+                res.first = peaks; res.second = i-k+2;
+            }
+        }
+        cout<<res.first+1<<" "<<res.second<<endl;
+    }
+	return 0;
+}
