742c091ed259f01f7af1d48db75720be0accbc9a
Ashish Jha <88213707+ashjha03@users.noreply.github.com>
2023-06-04 00:35:24 +0530

    Create DisjointSetClass.cpp

---
+++ DisjointSetClass.cpp
@@ -0,0 +1,32 @@
+class DisjointSet {
+    vector<int> parent, rank;
+  public: 
+    DisjointSet(int n){
+        parent.resize(n+1);
+        rank.resize(n+1, 0);
+
+        for(int i=0; i<n; i++)
+            parent[i] = i;
+    }
+
+    int findUParent(int node){
+        if(parent[node] == node)    return node;
+        return parent[node] = findUParent(parent[node]);
+    }
+
+    void unionByRank(int u, int v){
+        int ulp_u = findUParent(u);
+        int ulp_v = findUParent(v);
+
+        if(ulp_u == ulp_v)  return;
+
+        if(rank[ulp_u] < rank[ulp_v])
+            parent[ulp_u] = ulp_v;
+        else if(rank[ulp_u] > rank[ulp_v])
+            parent[ulp_v] = ulp_u;
+        else {
+            parent[ulp_u] = ulp_v;
+            rank[ulp_v]++;
+        }
+    }
+};
