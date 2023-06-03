f605f651bee85f0922f31f0c4099602e815211c3
Ashish Jha <88213707+ashjha03@users.noreply.github.com>
2023-06-04 00:41:35 +0530

    Update DisjointSetClass.cpp
    Union By Size Added

--- DisjointSetClass.cpp
+++ DisjointSetClass.cpp
@@ -1,9 +1,10 @@
 class DisjointSet {
-    vector<int> parent, rank;
+    vector<int> parent, rank, size;
   public: 
     DisjointSet(int n){
         parent.resize(n+1);
         rank.resize(n+1, 0);
+        size.resize(n+1, 0);
 
         for(int i=0; i<n; i++)
             parent[i] = i;
@@ -15,6 +16,22 @@ class DisjointSet {
     }
 
     void unionByRank(int u, int v){
+        int ulp_u = findUParent(u);
+        int ulp_v = findUParent(v);
+
+        if(ulp_u == ulp_v)  return;
+
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+
+    void unionBySize(int u, int v){
         int ulp_u = findUParent(u);
         int ulp_v = findUParent(v);
 
